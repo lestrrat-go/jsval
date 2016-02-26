4aeac8807ccad67cb68f157c87a5711ca6561e51
Daisuke Maki <lestrrat+github@gmail.com>
2016-02-26 13:55:05 +0900

    golint

--- bool.go
+++ bool.go
@@ -5,17 +5,20 @@ import (
 	"reflect"
 )
 
+// Boolean creates a new BooleanConsraint
 func Boolean() *BooleanConstraint {
 	return &BooleanConstraint{}
 }
 
+// Default specifies the default value to apply
 func (bc *BooleanConstraint) Default(v interface{}) *BooleanConstraint {
 	bc.defaultValue.initialized = true
 	bc.defaultValue.value = v
 	return bc
 }
 
-func (b *BooleanConstraint) Validate(v interface{}) error {
+// Validate vaidates the value against the given value
+func (bc *BooleanConstraint) Validate(v interface{}) error {
 	rv := reflect.ValueOf(v)
 	switch rv.Kind() {
 	case reflect.Bool:
