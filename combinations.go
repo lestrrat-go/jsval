bb859adc0bd84b760ec6b4de7eda5a50e3e32a85
Daisuke Maki <lestrrat+github@gmail.com>
2016-02-26 14:00:25 +0900

    golint

--- combinations.go
+++ combinations.go
@@ -18,19 +18,27 @@ func reduceCombined(cc interface{ Constraint; Constraints() []Constraint }) Cons
 	return cc
 }
 
+// Any creates a new AnyConstraint
 func Any() *AnyConstraint {
 	return &AnyConstraint{}
 }
 
+// Reduce returns the child Constraint, if this constraint
+// has only 1 child constraint.
 func (c *AnyConstraint) Reduce() Constraint {
 	return reduceCombined(c)
 }
 
+// Add appends a new Constraint
 func (c *AnyConstraint) Add(c2 Constraint) *AnyConstraint {
 	c.comboconstraint.Add(c2)
 	return c
 }
 
+// Validate validates the value against the input value.
+// For AnyConstraints, it will return success the moment
+// one child Constraint succeeds. It will return an error
+// if none of the child Constraints succeeds
 func (c *AnyConstraint) Validate(v interface{}) error {
 	for _, celem := range c.constraints {
 		if err := celem.Validate(v); err == nil {
@@ -40,19 +48,26 @@ func (c *AnyConstraint) Validate(v interface{}) error {
 	return errors.New("could not validate against any of the constraints")
 }
 
+// All creates a new AllConstraint
 func All() *AllConstraint {
 	return &AllConstraint{}
 }
 
+// Reduce returns the child Constraint, if this constraint
+// has only 1 child constraint.
 func (c *AllConstraint) Reduce() Constraint {
 	return reduceCombined(c)
 }
 
+// Add appends a new Constraint
 func (c *AllConstraint) Add(c2 Constraint) *AllConstraint {
 	c.comboconstraint.Add(c2)
 	return c
 }
 
+// Validate validates the value against the input value.
+// For AllConstraints, it will only return success if 
+// all of the child Constraints succeeded.
 func (c *AllConstraint) Validate(v interface{}) error {
 	for _, celem := range c.constraints {
 		if err := celem.Validate(v); err != nil {
@@ -62,19 +77,26 @@ func (c *AllConstraint) Validate(v interface{}) error {
 	return nil
 }
 
+// OneOf creates a new OneOfConstraint
 func OneOf() *OneOfConstraint {
 	return &OneOfConstraint{}
 }
 
+// Reduce returns the child Constraint, if this constraint
+// has only 1 child constraint.
 func (c *OneOfConstraint) Reduce() Constraint {
 	return reduceCombined(c)
 }
 
+// Add appends a new Constraint
 func (c *OneOfConstraint) Add(c2 Constraint) *OneOfConstraint {
 	c.comboconstraint.Add(c2)
 	return c
 }
 
+// Validate validates the value against the input value.
+// For OneOfConstraints, it will return success only if
+// exactly 1 child Constraint succeeds.
 func (c *OneOfConstraint) Validate(v interface{}) error {
 	count := 0
 	for _, celem := range c.constraints {
