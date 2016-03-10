0e19321503d479f26c1cb8d8e2afeecdd85c7627
Daisuke Maki <lestrrat+github@gmail.com>
2016-03-11 06:44:00 +0900

    tweak error message

--- array.go
+++ array.go
@@ -34,7 +34,7 @@ func (c *ArrayConstraint) Validate(v interface{}) (err error) {
 	switch rv.Kind() {
 	case reflect.Slice:
 	default:
-		return errors.New("value must be a slice")
+		return errors.New("value must be a slice (was: " + rv.Type().String() + ")")
 	}
 
 	l := rv.Len()
