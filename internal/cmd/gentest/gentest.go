c4794f229f6b36b08c02d286888099798ed3098b
Daisuke Maki <lestrrat+github@gmail.com>
2016-09-04 20:45:01 +0900

    Generate test code

---
+++ internal/cmd/gentest/gentest.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"log"
+	"os"
+
+	schema "github.com/lestrrat/go-jsschema"
+	jsval "github.com/lestrrat/go-jsval"
+	"github.com/lestrrat/go-jsval/builder"
+)
+
+func main() {
+	os.Exit(_main())
+}
+
+func _main() int {
+	s, err := schema.ReadFile(os.Args[1])
+	if err != nil {
+		log.Printf("%s", err)
+		return 1
+	}
+
+	b := builder.New()
+	v, err := b.Build(s)
+	if err != nil {
+		log.Printf("%s", err)
+		return 1
+	}
+
+	var out bytes.Buffer
+	out.WriteString("package jsval_test")
+	out.WriteString("\n\nimport \"github.com/lestrrat/go-jsval\"")
+	out.WriteString("\n")
+
+	g := jsval.NewGenerator()
+	if err := g.Process(&out, v); err != nil {
+		log.Printf("%s", err)
+		return 1
+	}
+
+	io.Copy(os.Stdout, &out)
+	return 0
+}
